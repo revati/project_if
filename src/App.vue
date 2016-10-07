45ed64b7e80c1aaa939401f919635e11fb8c0369
Roberts Gulans <rgulans@gmail.com>
2016-10-07 05:16:53 +0300

    Cars ifs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,54 @@
 <template>
+  <input v-model="car.horsePower" />
+  <br>
+
+  Plate number 
+  <span class="plateNumber">
+    {{ car.plateNumber }}
+  </span>
+
+  <br>
+  Horse power 
+  <span class="horsePower">
+    {{ car.horsePower }}
+  </span>
+  <br>
+
+  <span v-if="isCoolCar(car)">
+    Ahujenna
+  </span>
+
+  <span>Leta masina</span>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      car: {
+        plateNumber: "AA-3333",
+        horsePower: 1200,
+        price: 4000,
+      }
+    }
+  },
+  methods: {
+    isCoolCar (car) {
+      return (car.horsePower > 1000)
+    }
+  }
+}
 </script>
 
 <style>
+.plateNumber {
+  color: red;
+  font-weight: bold;
+}
+
+.horsePower {
+  color: blue;
+  font-weight: bold;
+}
+
 </style>
