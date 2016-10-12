f7b323272f90b89cf10e5c602726321bc5f2e61e
Roberts Gulans <rgulans@gmail.com>
2016-10-12 09:18:45 +0300

    Sort of login

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,35 @@
 <template>
-</templaste>
+  <input v-model="user.name">
+  <input type="password" v-model="user.pass">
+
+  <button @click="authorize(user.name, user.pass)">Log in</button>
+
+  <pre>{{ user | json }}</pre>
+</template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      user: {
+        name: "",
+        pass: ""
+      }
+    }
+  },
+  methods: {
+    authorize (username, password) {
+      console.log(password)
+      if (
+        username == "admin"
+        &&
+        password == "pass123"
+      ) {
+        alert("You are admin")
+      }
+    }
+  }
+}
 </script>
 
 <style>
