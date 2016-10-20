0cc1eea23cd2259d50272fd1ffe138a631742fc8
Roberts Gulans <rgulans@gmail.com>
2016-10-20 21:50:39 +0300

    Temperature

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,55 @@
 <template>
+  <div>
+    <input v-model="temperature">
+    <br>
+    <input 
+      id="farenheit"
+      type="radio" 
+      v-model="temeratureFormat" 
+      value="farenheit">
+    <label for="farenheit">Farenheit</label>
+    <input 
+      id="celsisus"
+      type="radio" 
+      v-model="temeratureFormat" 
+      value="celsisus">
+    <label for="celsisus">Celsius</label>
+
+
+    <br>
+    <temperature 
+      :temerature-format="temeratureFormat" 
+      :temperature="temperature">
+    </temperature>
+
+    <br>
+    <temperature 
+      :temerature-format="temeratureFormat" 
+      :temperature="temperature">
+    </temperature>
+
+    <br>
+    <temperature 
+      :temerature-format="temeratureFormat" 
+      :temperature="temperature">
+    </temperature>
+  </div>
 </template>
 
 <script>
+import Temperature from './components/Temperature'
+
+export default {
+  data () {
+    return {
+      temeratureFormat: "farenheit",
+      temperature: 32,
+    }
+  },
+  components: {
+    Temperature
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Temperature.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span v-if="temeratureFormat == 'celsisus'">{{ temperature }}</span>
+    <span v-if="temeratureFormat == 'farenheit'">{{ celsiusToFarenheit(temperature) }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['temperature', 'temeratureFormat'],
+  methods: {
+    celsiusToFarenheit: temp => {
+      if(temp.isInt) {
+        return temp * 1.8 + 32
+      }
+
+      return "Fuck off"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
