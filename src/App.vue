d0884b33921145ddf46d6539bac8f23312065d37
Roberts Gulans <rgulans@gmail.com>
2016-10-20 21:25:26 +0300

    Clean

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,7 @@
 <template>
-  <div v-if="user.isAdmin">
-    Es esmu admins
-    <b>{{ user.name }}</b>
-
-    <button @click="logout()">Logout</button>
-  </div>
-  <div v-if="!user.isAdmin">
-    Es esmu parasts lietotajs
-
-    <input v-model="user.name">
-    <input type="password" v-model="user.pass">
-
-    <button @click="authorize(user.name, user.pass)">Log in</button>
-  </div>
-
-  <pre>{{ user | json }}</pre>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      user: {
-        name: "",
-        pass: "",
-        isAdmin: false,
-      }
-    }
-  },
-  methods: {
-    authorize (username, password) {
-      if (username == "admin" && password == "pass123") {
-        this.user.isAdmin = true
-      } else {
-        this.user.pass = ""
-      }
-    },
-    logout () {
-      this.user.isAdmin = false
-    }
-  }
-}
 </script>
 
 <style>
